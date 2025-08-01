ecaf0cc100c20a987de18f16428a1f043b29a16f
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-01 20:29:11 +0500

    added 4 commit

---
+++ day-1/FirstTestCase.py
@@ -0,0 +1,9 @@
+# 1) Open Web Browser(Chrome/firefox/IE).
+# 2) Open URL https://admin-demo.nopcommerce.com/login
+# 3) provide Email (admin@yourstore.com).
+# 4) Provide Password (admin).
+# 5) Click on Login.
+# 6) Capture title of the dashboard page.(Actual title)
+# 7) Verify tilte of the page: "Dashboard / nopCommerce administration" (Exected)
+# 8) close browser
+
