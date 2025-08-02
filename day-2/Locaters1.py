6440090a4d53fb24afa2dfb0b0b76bd04f1a08d1
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-02 22:51:59 +0500

    added 28 commit

--- day-2/Locaters1.py
+++ day-2/Locaters1.py
@@ -1,1 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.service import Service
+
+service = Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+
+driver = webdriver.Chrome(service=service)
