91ef43b7652884388c3e300fa621aa3ba8a7cbee
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-02 05:42:36 +0500

    added 33 commit

--- day-1/edge.py
+++ day-1/edge.py
@@ -8,7 +8,10 @@ service = EdgeService(r"C:\Drivers\edgedriver_win64\msedgedriver.exe")
 driver = webdriver.Edge(service=service)
 
 WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.NAME))
+    EC.visibility_of_element_located((By.NAME, "username"))
 )
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
+driver.find_element(By.NAME, "username").send_keys("Admin")
+driver.find_element(By.NAME, "password").send_keys("admin123")
+driver.find_element(By.CSS_SELECTOR, "button").send_keys("admin123")
