b6777ee97d923787f748e692ccc7ec8ba1893abc
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-03 01:50:13 +0500

    added 35 commit

--- day-2/Locaters1.py
+++ day-2/Locaters1.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.service import Service
+from selenium.webdriver.chrome.service import Service
 
-ser_obj = Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+ser_obj = Service(executable_path=r"C:\Drivers\chromedriver-win64\chromedriver.exe")
 
 driver = webdriver.Chrome(service=ser_obj)
 
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()
 
-driver.find_element(By.ID, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
+driver.find_element(By.NAME, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
+driver.find_element(By.ID, "q").send_keys("Lenovo Thinkpad Carbon Laptop")
