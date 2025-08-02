54d265f30199870b8978d722c043dab715ebe6af
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-02 22:56:01 +0500

    added 30 commit

--- day-2/Locaters1.py
+++ day-2/Locaters1.py
@@ -4,3 +4,5 @@ from selenium.webdriver.common.service import Service
 ser_obj = Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
 
 driver = webdriver.Chrome(ser_obj)
+
+driver.get("https://demo.nopcommerce.com/")
