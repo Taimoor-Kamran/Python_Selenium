881ab3a92d9713abfa8c71203c9d9e9fdd3f7e92
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-02 05:20:10 +0500

    added 23 commit

--- day-1/FireFox.py
+++ day-1/FireFox.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium import webdriver
+from selenium import webdriver
 
-service = FirefoxService(r"C:\Drivers\geckodriver-v0.34.0-win64\geckodriver.exe")
+service = FirefoxService("C:\Drivers\geckodriver-v0.34.0-win64\geckodriver.exe")
+
 driver = webdriver.Firefox(service=service)
 
-driver.get("https://opensource-demo.orangehrmlive.com/")
-driver.find_element(By.NAME, "username").send_keys("Admin")
-driver.find_element(By.NAME, "password").send_keys("admin123")
-driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-
-driver.close()
