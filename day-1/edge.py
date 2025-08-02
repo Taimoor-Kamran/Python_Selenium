a81ca2ab9db9480f17239ddd7ed67a9a8a796a64
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-02 05:40:02 +0500

    added 31 commit

--- day-1/edge.py
+++ day-1/edge.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait 
 
 service = EdgeService(r"C:\Drivers\edgedriver_win64\msedgedriver.exe")
-
 driver = webdriver.Edge(service=service)
+
+WebDriverWait(driver, 10).until(
+    
+)
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
