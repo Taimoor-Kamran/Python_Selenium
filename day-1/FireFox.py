74d5bc398d28e5f398ca80f1a80232d8358e767f
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-02 05:22:20 +0500

    added 25 commit

--- day-1/FireFox.py
+++ day-1/FireFox.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium import webdriver
 
@@ -9,3 +9,6 @@ service = FirefoxService("C:\Drivers\geckodriver-v0.34.0-win64\geckodriver.exe")
 driver = webdriver.Firefox(service=service)
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
+driver.find_element(By.NAME, "username").send_keys("Admin")
+driver.find_element(By.NAME, "password").send_keys("admin123")
+driver.find_element(By.NAME, "username").send_keys("Admin")
