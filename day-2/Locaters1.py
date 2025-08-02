bbf0c42dfd1bf15cef1c55586167be3d4245b787
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-03 01:34:52 +0500

    added 32 commit

--- day-2/Locaters1.py
+++ day-2/Locaters1.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.service import Service
 
 ser_obj = Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
@@ -7,3 +8,5 @@ driver = webdriver.Chrome(ser_obj)
 
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()
+
+driver.find_element(By.ID, "small-searchterms")
