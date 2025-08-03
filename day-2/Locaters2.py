4bcc77b0984d5bd476621c2fa243da0b56079939
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-03 16:59:22 +0500

    added 46 commit

---
+++ day-2/Locaters2.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service(executable_path=r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://demo.nopcommerce.com/")
+driver.maximize_window()
+
+# Wait for the search bar to appear
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, "q"))
+)
+
+# Id & Name Locaters
+
+# driver.find_element(By.NAME, "q").send_keys("Lenovo Thinkpad Carbon Laptop")
+# driver.find_element(By.ID, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
+
+# LinkText and Partial LinkText
+
+# driver.find_element(By.LINK_TEXT, "Register").click()
+# driver.find_element(By.PARTIAL_LINK_TEXT, "Reg").click()
+
+links = driver.find_elements(By.TAG_NAME, "a")
+print(len(links)) # total numbers of links on home page
