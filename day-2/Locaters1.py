0557d0a6a60cb581463245a0544d2e4f44e759e5
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-03 15:16:37 +0500

    added 36 commit

--- day-2/Locaters1.py
+++ day-2/Locaters1.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-ser_obj = Service(executable_path=r"C:\Drivers\chromedriver-win64\chromedriver.exe")
-
-driver = webdriver.Chrome(service=ser_obj)
+service = Service(executable_path=r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
 
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()
 
-driver.find_element(By.NAME, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
-driver.find_element(By.ID, "q").send_keys("Lenovo Thinkpad Carbon Laptop")
+# Wait for the search bar to appear
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, "q"))
+)
+
+# Send input using the correct selector
+# driver.find_element(By.NAME, "q").send_keys("Lenovo Thinkpad Carbon Laptop")
+# driver.find_element(By.ID, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
