a93979a4557d3720dd58ba81e2fe15011a8f9643
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-04 01:59:12 +0500

    added 65 commit

--- Practice_Folder/Form.py
+++ Practice_Folder/Form.py
@@ -4,13 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-ser_obj = Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+# Setup
+service = Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
 
-driver = webdriver.Chrome(service=ser_obj)
 driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdQPxfXwDysopvaWiNfzfaAD6hjuFquw-xeGh8TZpMF4roWfQ/viewform")
+driver.maximize_window()
 
-WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, "input.whsOnd"))
-)
+inputs = driver.find_element(By.CSS_SELECTOR, "input.whsOnd")
 
-input = driver.find_element(By.CSS_SELECTOR, 'input.whsOnd')
+inputs[0].send_keys("Taimoor Kamran")
+inputs[1].send_keys("Kamran Sattar")
+inputs[2].send_keys("12345")
+inputs[3].send_keys("03413739780")
+inputs[4].send_keys("taimoor@gmail.com")
