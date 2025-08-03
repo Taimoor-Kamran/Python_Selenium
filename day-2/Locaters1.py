b90b2f962b08c20511969de5353f10b0a8cf30c8
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-03 15:17:23 +0500

    added 37 commit

--- day-2/Locaters1.py
+++ day-2/Locaters1.py
@@ -15,6 +15,10 @@ WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.NAME, "q"))
 )
 
-# Send input using the correct selector
+# Id & Name Locaters
+
 # driver.find_element(By.NAME, "q").send_keys("Lenovo Thinkpad Carbon Laptop")
 # driver.find_element(By.ID, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
+
+# LinkText and Partial LinkText
+
