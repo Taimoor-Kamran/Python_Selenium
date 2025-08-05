637eb68f3b2e5273a4fe593a8d7d249d85085667
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-05 17:28:00 +0500

    Fixed chromedriver path for linux jenkins

--- Practice_Folder/Form.py
+++ Practice_Folder/Form.py
@@ -4,27 +4,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Setup ChromeDriver
-service = Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+service = Service(r"/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdQPxfXwDysopvaWiNfzfaAD6hjuFquw-xeGh8TZpMF4roWfQ/viewform")
 driver.maximize_window()
 
-# Wait until the first input field is visible
 WebDriverWait(driver, 10).until(
     EC.visibility_of_element_located((By.CSS_SELECTOR, "input.whsOnd"))
 )
 
-# Find and fill all input fields
 inputs = driver.find_elements(By.CSS_SELECTOR, "input.whsOnd")
 
-inputs[0].send_keys("Taimoor Kamran")        # Full Name
-inputs[1].send_keys("Kamran Sattar")         # Father Name
-inputs[2].send_keys("12345")                 # ID
-inputs[3].send_keys("37")                    # Age or Roll No.
-inputs[4].send_keys("03413739780")           # Phone
-inputs[5].send_keys("taimoor@gmail.com")     # Email
+inputs[0].send_keys("Taimoor Kamran")         
+inputs[1].send_keys("Kamran Sattar")          
+inputs[2].send_keys("12345")                 
+inputs[3].send_keys("37")                     
+inputs[4].send_keys("03413739780")           
+inputs[5].send_keys("taimoor@gmail.com")     
 
 driver.find_element(By.XPATH, '//span[text()="Submit"]').click()
 
